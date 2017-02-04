6e82c4235e08d65a69a4e0be52ee2e88092acb60
seshu <papolu.cloud@outlook.com>
2017-02-04 23:14:22 +0530

    Added StringExtensions

---
+++ ExtensionMethods/Seshu.Playground.Extensions/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Seshu.Playground.Extensions
+{
+    /// <summary>
+    /// StringExtensions class contains the extension methods for  string
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Gets the string by in UTC format
+        /// </summary>
+        /// <param name="input">The input</param>
+        /// <returns>string</returns>
+        public static string ToUtcShortdateString(this string input, string format)
+        {
+            if (string.IsNullOrEmpty(input))
+                return null;
+            string parsedDate = null;
+            try
+            {
+                DateTime date;
+                if (DateTime.TryParseExact(input, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+                    parsedDate = date.ToShortDateString();
+            }
+            catch
+            {
+                // ignored
+            }
+            return parsedDate;
+        }
+    }
+}
---
+++ ExtensionMethods/Seshu.Playground.UnitTests/ExtensionMethods Test/StringExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Seshu.Playground.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seshu.Playground.UnitTests.ExtensionMethods_Test
+{
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        [TestMethod]
+        public void ToUtcShortdateString_Should_Return_Valid_Date()
+        {
+            string utcDateFormat = "ddd MMM dd HH:mm:ss UTC yyyy";
+            var date = StringExtensions.ToUtcShortdateString("Sun Feb 10 14:07:00 UTC 2013", utcDateFormat);
+            Assert.AreEqual("2/10/2013", date);
+        }
+
+    }
+}
