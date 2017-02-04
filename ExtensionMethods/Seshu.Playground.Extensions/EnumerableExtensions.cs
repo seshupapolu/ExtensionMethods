09838feead702c63dcc10e8affae59cf950ba97b
seshu <papolu.cloud@outlook.com>
2017-02-04 23:57:39 +0530

    Added Enumerable Extensions

---
+++ ExtensionMethods/Seshu.Playground.Extensions/EnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seshu.Playground.Extensions
+{
+
+    /// <summary>
+    /// EnumerableExtensions class contains the extension methods for  IEnumerable<string/>
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Gets the string by joining the string
+        /// </summary>
+        /// <param name="list">The input</param>
+        /// <param name="seperator">The Seperator</param>
+        /// <returns>string</returns>
+        public static string Join(this IEnumerable<string> list, char seperator)
+        {
+            if (list == null || !list.Any()) return null;
+
+            StringBuilder builder = new StringBuilder();
+            foreach (string item in list)
+            {
+                builder.Append(item);
+                builder.Append(seperator);
+            }
+            return builder.ToString().Substring(0, builder.Length - 1);
+        }
+    }
+
+}
---
+++ ExtensionMethods/Seshu.Playground.UnitTests/ExtensionMethods Test/EnumerableExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Seshu.Playground.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seshu.Playground.UnitTests.ExtensionMethods_Test
+{
+
+    [TestClass]
+    public class EnumerableExtensionsTests
+    {
+        [TestMethod]
+        public void Join_Should_Return_Valid_String()
+        {
+            var listOfStrings = new List<string>
+            {
+                "Seshu",
+                "Papolu"
+            };
+
+            var result= EnumerableExtensions.Join(listOfStrings,',');
+            Assert.AreEqual("Seshu,Papolu", result);
+
+        }
+    }
+}
