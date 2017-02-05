b7c2871d461f6a2905d98e321376f1ca6e3fd27e
seshu <papolu.cloud@outlook.com>
2017-02-05 20:08:36 +0530

    Removed unused namespaces

--- ExtensionMethods/Seshu.Playground.UnitTests/ExtensionMethods Test/DateTimeExtensionsTest.cs
+++ ExtensionMethods/Seshu.Playground.UnitTests/ExtensionMethods Test/DateTimeExtensionsTest.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Seshu.Playground.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Seshu.Playground.UnitTests.ExtensionMethods_Test
 {
